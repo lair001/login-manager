5dddc1da50ad478a2833c9f16d549d5064cba3e9
Samuel Lair <lair001@gmail.com>
2018-06-03 15:12:19 -0400

    Implement ActionStore

---
+++ login_manager/libs/ActionStore.py
@@ -0,0 +1,46 @@
+class ActionStore:
+
+    def __init__(self):
+        self.shoulds = {}
+
+    def set_should_create_as_true(self):
+        self.__set_should_as_true(self, 'create')
+
+    def set_should_create_as_false(self):
+        self.__set_should_as_false(self, 'create')
+
+    def set_should_load_as_true(self):
+        self.__set_should_as_true(self, 'load')
+
+    def set_should_load_as_false(self):
+        self.__set_should_as_false(self, 'load')
+
+    def set_should_designate_default_as_true(self):
+        self.__set_should_as_true(self, 'designate_default')
+
+    def set_should_designate_default_as_false(self):
+        self.__set_should_as_false(self, 'designate_default')
+
+    def should_create(self):
+        return self.__should(self, 'create')
+
+    def should_load(self):
+        return self.__should(self, 'load')
+
+    def should_designate_default(self):
+        return self.__should(self, 'designate_default')
+
+    # private methods
+
+    def __set_should(self, action, boolean):
+        self.shoulds[action] = boolean
+
+    def __set_should_as_true(self, action):
+        self.__set_should(action, True)
+
+    def __set_should_as_false(self, action):
+        self.__set_should(action, False)
+
+    def __should(self, action):
+        return self.shoulds[action]
+
