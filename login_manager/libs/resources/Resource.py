61678ea58da8331bfd0da422c9e599692947073b
Samuel Lair <lair001@gmail.com>
2018-06-11 21:55:48 -0400

    Implement abstract Resource methods ...
    is_valid and validate

--- login_manager/libs/resources/Resource.py
+++ login_manager/libs/resources/Resource.py
@@ -1,6 +1,7 @@
 from login_manager.libs.metaclasses.AbstractSingleton import AbstractSingleton
 from abc import abstractmethod
 
+
 class Resource(metaclass=AbstractSingleton):
 
     @abstractmethod
@@ -14,6 +15,20 @@ class Resource(metaclass=AbstractSingleton):
     @abstractmethod
     def designate_default(self):
        self.__class__.__default_action('designate_default')
+
+    @classmethod
+    @abstractmethod
+    def is_valid(cls):
+        cls.__default_method()
+
+    @classmethod
+    @abstractmethod
+    def validate(cls):
+        cls.__default_method()
+
+    @classmethod
+    def __default_method(cls):
+        raise NotImplementedError
 
     @classmethod
     def __default_action(cls, action):
--- login_manager/libs/resources/Username.py
+++ login_manager/libs/resources/Username.py
@@ -1,3 +1,5 @@
+import sys
+import re
 from login_manager.libs.resources.Resource import Resource
 
 
@@ -17,3 +19,17 @@ class Username(Resource):
 
     def designate_default(self, username_str):
         self.load(username_str)
+
+    __valid_username_regex = re.compile('\A[^@/~]{1,32}\Z')
+
+    @classmethod
+    def is_valid(cls, username_str):
+        return re.match(cls.__valid_username_regex, username_str) is not None
+
+    @classmethod
+    def validate(cls, username_str):
+        if cls.is_valid(username_str):
+            return username_str
+        else:
+            print(f"The provided username is invalid: {username_str}")
+            sys.exit(2)
