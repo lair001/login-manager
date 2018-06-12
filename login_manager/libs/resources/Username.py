f95e9d987fa4d9679bab54c0a93f28f38922a299
Samuel Lair <lair001@gmail.com>
2018-06-11 21:53:34 -0400

    Rename username attribute of Username to username_str

--- login_manager/libs/resources/Username.py
+++ login_manager/libs/resources/Username.py
@@ -6,11 +6,11 @@ class Username(Resource):
     def __init__(self):
         pass
 
-    def load(self, username):
-        self.username = username
+    def load(self, username_str):
+        self.username_str = self.__class__.validate(username_str)
 
     def create(self):
         super().create()
 
-    def designate_default(self, username):
-        self.load(username)
+    def designate_default(self, username_str):
+        self.load(username_str)
