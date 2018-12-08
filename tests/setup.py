30ed93057b24891f7775b488e16893584e4ddf4b
Samuel Lair <lair001@gmail.com>
2018-12-07 23:55:17 -0500

    Correct url for test package

--- tests/setup.py
+++ tests/setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author_email="lair001@gmail.com",
     version="0.0.1",
     license="MIT",
-    url="https://github.com/lair001/data-structures-and-algorithms-in-python-solutions",
+    url="https://github.com/lair001/login-manager",
     description="Test suite for Login Manager.",
     packages=setuptools.find_packages()
 )
